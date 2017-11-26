aea7cfdc39ef4615f1237044aae4a4b835b047ce
sujaytalesara <sujay.talesara@gmail.com>
2017-11-26 01:12:28 +0000

    Basic web.py implementation - Rest

--- fileServer.py
+++ fileServer.py
@@ -4,52 +4,16 @@
 description : Distributed file system
 """
 
-import socket
-import threading as th
-import os
+import web
 
-def retriveFile(name,sock):
-     filename = sock.recv(1024)
-     if os.path.isfile(filename):
-          file = str(b'filename')
-          print(file + "****")
-          fileSize = str(os.path.getsize('main.py'))
-          
-          sock.send("Exits" + fileSize )
-          #--Receive User response
-          userResponce = sock.recv(1024)
-          # takes user input
-          if userResponce[:2] == 'OK':
-               # Open file as read binary
-               with open(filename,'rb') as f: 
-                    bytesToSend = f.read(1024)
-                    while bytesToSend != "":
-                         bytesToSend = f.read(1024)
-                         sock.send(bytesToSend)
-                    
-     else:
-          sock.send("File Does not Exists --- Error")
-          sock.close()
-          
-def Main():
-     host = '127.0.0.1'
-     port = 3000
-     
-     s = socket.socket()
-     # binding socket obj with host & port no
-     s.bind((host,port))
-     # Server starts listening to connnection
-     s.listen(5)               
-     
-     print ("Server Started")
-     while True:
-          # Connection socket & address
-          conn,addr = s.accept()
-          print ("client connected ip<:" + str(addr) + ">")
-          t = th.Thread(target = retriveFile, args = ('retrive Thread',conn))
-          t.start()
-     s.close()
-     
-if __name__ == '__main__':
-     Main()
-     
+urls = ('/(.+)','Index')
+
+class Index:
+	def GET(self,name=None):
+		return ("Hello World !" + name)
+	def POST(self,name):
+		return "post"
+
+if __name__=="__main__":
+	app = web.application(urls,globals())
+	app.run()
