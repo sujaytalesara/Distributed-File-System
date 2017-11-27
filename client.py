9612bfc6fb0c71a5a9210492e4af275bf18583c3
sujaytalesara <sujay.talesara@gmail.com>
2017-11-27 20:59:07 +0000

    Directory server

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
 import requests as req
 
-url = "http://localhost:8080/filepath/"
+#url = "http://localhost:8080/fileserver/"
+url = "http://localhost:8080/directoryserver/"
 
 filePath = input("Enter File Name : ")
+
 url = url + filePath
 response = req.get(url)
-#print(filePath)
-#print(url)
+#response = req.get(filePath)
+
+print(filePath + "....")
 print("Response is ready: " + response.text)
+print("Response is ready: " + str(response))
