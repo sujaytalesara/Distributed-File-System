3cdb15ecbd73fa0d2a7022123655e3d023610c89
sujaytalesara <sujay.talesara@gmail.com>
2017-12-14 13:20:56 +0000

    Lock Server

---
+++ lockServer.py
@@ -0,0 +1,48 @@
+
+import PortManager
+import shelve
+
+
+urls = ('/lockServer/fileName/(.*)','lockServer',
+'/lockServer/fileName/Unlock/(.*)','lockServer2')
+
+class lockServer:
+
+     def GET(self,fileName):
+         print(fileName)
+         shelv = shelve.open("locket_Directory.dat")
+         value = shelv[fileName]
+         print(value)
+         try:
+             if value == {0}:
+                 shelv[fileName] = {1}
+                 print("***********************Success ************************")
+                 return 'FileLocked'
+             elif value == {1}:
+                 print("***********************else ************************")
+                 return 'Not'
+         except:
+             print('Error while locking')
+         finally:
+             shelv.close()
+
+     def POST(self,fileName):
+          print(fileName)
+          shelv = shelve.open("locket_Directory.dat")
+          value = shelv[fileName]
+          #print(value)
+          try:
+              if value == {1}:
+                  shelv[fileName] = {0}
+                  print("*******Success ***********")
+              return 'FileUnLocked'
+          except:
+              print('Error while locking')
+          finally:
+              shelv.close()
+
+
+
+if __name__=="__main__":
+	app = PortManager.changePort(urls,globals())
+	app.run(port=8083)
